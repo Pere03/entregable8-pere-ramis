44f6a46775b935c7b672278e57a808943b69d6d0
Pere03 <pereramis03@gmail.com>
2022-03-16 12:43:24 +0100

    16 - 3 - 2022

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip Audio1;
+    public AudioClip Audio2;
+    public AudioClip Audio3;
+    public AudioClip Audio4;
+    public AudioClip Audio5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
